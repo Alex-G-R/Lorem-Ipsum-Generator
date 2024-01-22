512c1a5d0819899614ae1532480278921bcc5980
Alex <alex.grabski555@gmail.com>
2024-01-22 18:30:42 +0100

    it is moving

--- functions.hpp
+++ functions.hpp
@@ -1,2 +1,5 @@
 
 void greet_user();
+int setup();
+int get_number_input();
+void new_line();
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main()
 {
     greet_user();
-
+    setup();
 
     return 0;
 }
@@ -16,4 +16,24 @@ void greet_user()
 {
     std::cout << "Hey, this is a lorem text generator." << std::endl;
     std::cout << "Text will be random everytime!" << std::endl;
+    new_line();
+}
+
+int setup()
+{
+    std::cout << "Please provide the number of words you want to generate: ";
+    int number_of_words = get_number_input();
+    new_line();
+}
+
+int get_number_input()
+{
+    int number;
+    std::cin >> number;
+    return number;
+}
+
+void new_line()
+{
+    std::cout << "" << std::endl;
 }

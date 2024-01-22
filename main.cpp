a4fa099a73856eaa8476f8493f00a4d1ea0d9f2d
Alex <alex.grabski555@gmail.com>
2024-01-22 18:25:33 +0100

    starting

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@
 
 int main()
 {
-
+    greet_user();
 
 
     return 0;
+}
+
+void greet_user()
+{
+    std::cout << "Hey, this is a lorem text generator." << std::endl;
+    std::cout << "Text will be random everytime!" << std::endl;
 }
